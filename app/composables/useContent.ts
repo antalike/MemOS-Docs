1fbad8e6652dfb1b01ffbcfbba546f1323b9ac99
harvey_xiang <harvey_xiang22@163.com>
2025-09-20 17:24:26 +0800

    feat: add auto br

--- app/composables/useContent.ts
+++ app/composables/useContent.ts
@@ -1,24 +1,30 @@
 export function useContent(pageValue: { body: { value: Array<[string, object]> }, path: string }) {
-  const { body, path } = pageValue
+  try {
+    const { body, path } = pageValue
 
-  if (path.includes('open_source')) {
+    if (path.includes('open_source')) {
+      return pageValue
+    }
+
+    const newBodyValue: Array<[string, object]> = []
+    body.value.forEach((item: [string, object], index: number) => {
+      if (index === 0) {
+        newBodyValue.push(item)
+      } else {
+        const preValue = body.value[index - 1]
+        if (['h2', 'h3', 'h4'].includes(item[0]) && !['h2', 'h3', 'h4'].includes(preValue[0])) {
+          newBodyValue.push(['br', {}])
+        }
+
+        newBodyValue.push(item)
+      }
+    })
+
+    pageValue.body.value = newBodyValue
+
+    return pageValue
+  } catch (error) {
+    console.error('Error in useContent:', error)
     return pageValue
   }
-
-  const newBodyValue: Array<[string, object]> = []
-  body.value.forEach((item: [string, object], index: number) => {
-    if (index === 0) {
-      newBodyValue.push(item)
-    } else {
-      if (['h2', 'h3', 'h4'].includes(item[0])) {
-        newBodyValue.push(['br', {}])
-      }
-
-      newBodyValue.push(item)
-    }
-  })
-
-  pageValue.body.value = newBodyValue
-
-  return pageValue
 }
