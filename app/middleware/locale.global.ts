7b652ece594b9ad98a9c78779b21bdcefccc3be0
harvey_xiang <harvey_xiang22@163.com>
2025-11-11 20:58:14 +0800

    feat: add locale middleware

--- app/middleware/locale.global.ts
+++ app/middleware/locale.global.ts
@@ -11,10 +11,12 @@ export default defineNuxtRouteMiddleware((to, from) => {
   const { locale } = useNuxtApp().$i18n
 
   if (locale.value === 'en' && to.path.startsWith('/cn')) {
+    console.log('Redirect from cn path to en path')
     return navigateTo(to.path.replace('/cn', ''))
   }
 
   if (locale.value === 'cn' && !to.path.startsWith('/cn')) {
+    console.log('Redirect from en path to cn path')
     return navigateTo(`/${locale.value}${to.path}`)
   }
 })
