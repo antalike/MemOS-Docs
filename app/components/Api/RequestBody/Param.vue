24b03ab733bb1a55fd114b2da3b77e6a374941cf
antalike <>
2025-12-18 17:46:50 +0800

    refactor: update api_docs/api.json

--- app/components/Api/RequestBody/Param.vue
+++ app/components/Api/RequestBody/Param.vue
@@ -12,6 +12,9 @@ const properties = computed(() => {
   if (props.schema.anyOf) {
     return props.schema.anyOf.filter(item => Object.prototype.hasOwnProperty.call(item, 'properties'))?.[0]?.properties
   }
+  if (props.schema.oneOf) {
+    return props.schema.oneOf.filter(item => Object.prototype.hasOwnProperty.call(item, 'properties'))?.[0]?.properties
+  }
   return null
 })
 
@@ -58,6 +61,11 @@ function isRequired(list: string[] | undefined | null, prop: string) {
         <ApiRequestBodyArrayParam
           v-if="schema.anyOf?.length"
           :any-of="schema.anyOf"
+        />
+        <!-- Handle oneOf -->
+        <ApiRequestBodyArrayParam
+          v-if="schema.oneOf?.length"
+          :any-of="schema.oneOf"
         />
         <!-- Handle Items -->
         <ApiRequestBodyArrayParam
