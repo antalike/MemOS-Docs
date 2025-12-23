bb8d75a6b6566d3babd04084f833c95d7f67a744
harvey_xiang <harvey_xiang22@163.com>
2025-12-23 21:35:30 +0800

    feat: update env

--- envConfig/config.pre.ts
+++ envConfig/config.pre.ts
@@ -1,7 +1,7 @@
 export default {
   env: 'pre',
   enDomain: '',
-  homeDomain: 'https://memos-pre.memtensor.com.cn',
+  homeDomain: 'https://memos-pre.openmem.net',
   openMemUrl: 'https://pre.openmem.net',
   githubMemosUrl: 'https://github.com/MemTensor/MemOS',
   dashboardUrl: 'https://memos-dashboard-pre.openmem.net'
